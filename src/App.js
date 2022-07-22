ec4866e8bcb0d0095e7cf9d5512e7508caa8b770
rohitp20092 <rohitp20092@gmail.com>
2022-07-22 15:38:08 +0530

    implemented form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+
+  const [email,setEmail] = useState("")
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleForm = (e) => {
+    console.log("e",email,username,password)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form> 
+    <div className="abc">
+      <div className="container">
+      <label ><b>Email</b></label>
+      <input type="text" placeholder="Enter Email" name="email" value={email}  onChange={(e) => setEmail(e.target.value)}/>
+      <label ><b>username</b></label>
+      <input type="text" placeholder="Enter Username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+
+      <label ><b>Password</b></label>
+      <input type="current-password" placeholder="Enter Password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button type="submit" onClick={handleForm}>Login</button>
+      <label>
+      </label>
     </div>
+    </div>
+    </form>
   );
 }
 
 export default App;
+
